61180f3efe1afd1330a7ade7726f8d19e1703e21
jcoronado1 <jss.coronado@hotmail.com>
2018-12-07 14:16:26 -0500

    acrtualizando navegacion

---
+++ src/entries/app.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from '../reducers/index';
+import Routes from './router.js';
+import { BrowserRouter } from 'react-router-dom';
+const store = createStore(
+    reducer,
+    {},
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
+const homeContainer = document.getElementById('app');
+render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <Routes/>
+    </Provider>
+  </BrowserRouter>
+, homeContainer);
