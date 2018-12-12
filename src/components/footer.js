66aead03ec9eaeffb76ded3aa2cdff0a52eb9f1b
jcoronado1 <jss.coronado@hotmail.com>
2018-12-12 17:20:59 -0500

    ajuste de footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,41 +6,36 @@ class Footer extends Component{
     render(){
         return(
             <div className="item footer">
-            <nav>
-                    <ul>
-                        <li>
-                            <NavLink to="/Info" activeClassName="is-selected">
-                                Information
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/Fees_and_commissions" activeClassName="is-selected">
-                                Fees and Commissions
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/Simulator" activeClassName="is-selected">
-                                Loan Simulator
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/Chat" activeClassName="is-selected">
-                                Chat
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/Contact" activeClassName="is-selected">
-                                Contact
-                            </NavLink>
-                        </li>
-                        
-                        <li>
-                            <NavLink to="/About" activeClassName="is-selected">
-                                About us
-                            </NavLink>
-                        </li>
-                    </ul>
-                </nav>
+                <diV>
+                     <NavLink to="/Info" activeClassName="is-selected">
+                        Information
+                    </NavLink>
+                </diV>
+                <diV>
+                    <NavLink to="/Fees_and_commissions" activeClassName="is-selected">
+                        Fees and Commissions
+                    </NavLink>
+                </diV>
+                <diV>
+                    <NavLink to="/Simulator" activeClassName="is-selected">
+                        Loan Simulator
+                    </NavLink>
+                </diV>
+                <diV>
+                    <NavLink to="/Chat" activeClassName="is-selected">
+                        Chat
+                    </NavLink>
+                </diV>
+                <diV>
+                    <NavLink to="/Contact" activeClassName="is-selected">
+                        Contact
+                    </NavLink>
+                </diV>
+                <diV>
+                   <NavLink to="/About" activeClassName="is-selected">
+                        About us
+                    </NavLink>
+                </diV>
             </div>    
             )
     }
