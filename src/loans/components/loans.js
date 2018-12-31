b81b115315637b693b22f8af183dc6a7de120e23
jcoronado1 <email.coronado@gmail.com>
2018-12-31 14:13:29 -0500

    listados del lend 1

--- src/loans/components/loans.js
+++ src/loans/components/loans.js
@@ -4,8 +4,23 @@ import Grid from '../../widgets/grid/components/grid';
 class Loans extends Component{
     constructor(props){
         super(props);
-       let data={campos:['nombre','apellido','cedula','precio','color'],anchos:['20px','50px','cedula','precio','color']}
-       //alert(data);
+       
+        let result=[{
+                    data: ['Jesús Alberto Coronado','Loan with guarantee','Have a good reputation','$10.000.000',' borrow']
+                   },{
+                    data: ['Jesús Alberto Coronado','Loan with guarantee','Have a good reputation','$10.000.000','borrow']
+                   },{
+                    data: ['Jesús Alberto Coronado','Loan with guarantee','Have a good reputation','$10.000.000','borrow']
+                   },{
+                    data: ['Jesús Alberto Coronado','Loan with guarantee','Have a good reputation','$10.000.000','borrow']
+                   }];
+        let data={
+            title:['Lender','Loan Method','Requirements','Amount',''],
+            data:result
+        };
+       this.state={
+        data: data
+       }
     }
    
     render(){
@@ -15,38 +30,9 @@ class Loans extends Component{
                     Borrows money in Bogota
                 </div>
                 <div>
-                    <Grid data={true}/>
+                    <Grid data={this.state.data}/>
                 </div>
-                <div className="appTitle marginGrid2">
-                    Exchange of loans in Bogota
-                </div>
-                <div>
-                    <Grid data={true}/>
-                </div>
-                <div>
-                    <div>
-                        <div className="appTitle">
-                            Top of lenders
-                        </div>
-                        <div>
-                            <Grid data={true}/>
-                        </div>
-                    </div>
-                    <div>
-                        <div className="appTitle">
-                            Top of customers
-                        </div>
-                        <div>
-                            <Grid data={true}/>
-                        </div>
-                    </div>
-                </div>
-                <div className="appTitle marginGrid2">
-                   Last loans
-                </div>
-                <div>
-                    <Grid data={true}/>
-                </div>
+                
                 
             </div>  
             
--- src/widgets/grid/components/grid.js
+++ src/widgets/grid/components/grid.js
@@ -2,149 +2,38 @@ import React, { Component} from 'react';
 import '../css/grid.css';
 
 class Grid extends Component{
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
+        
+        
         let htmlStyles = window.getComputedStyle(document.querySelector("html"));
         document.documentElement.style.setProperty("--columns", 5);
         document.documentElement.style.setProperty("--rows", 5);
     }
+    createTable = () => {
+        let result = []
+        let data = this.props.data.data;
+        console.log(data);
+        // Outer loop to create parent
+        for (let i = 0; i < data.length; i++) {
+            for (let j = 0; j < data[i].data.length; j++) {
+                result.push( <div className="grid-td"><div className="grid-td-padd">{data[i].data[j]}</div></div>)
+            }
+        }
+        return result
+    }
     render(){
+        let title = this.props.data.title;
+        let data = this.props.data.data;
         return(
          <div className="contenedor">
             <div className="grid">
                 <div className="grid-content-th">
-                    <div className="grid-th">
-                        <div className="grid-th-padd">
-                            Lender    
-                        </div>
-                    </div>
-                    <div className="grid-th">
-                        <div className="grid-th-padd">
-                            Loan Method     
-                        </div>
-                    </div>
-                    <div className="grid-th">
-                        <div className="grid-th-padd">
-                            Requirements     
-                        </div>
-                    </div>
-                    <div className="grid-th">
-                        <div className="grid-th-padd">
-                            Amount    
-                        </div>
-                    </div>
-                    <div className="grid-th">
-                        
-                    </div>
+                     {title.map(title => <div className="grid-th"><div className="grid-th-padd">{title}</div></div>)}
                 </div>
-                <div className="grid-content-td nuevaclase" id="contenido">
-                    <div className="grid-td">
-                        <div className="grid-td-padd">
-                            Jesús Alberto Coronado       
-                        </div>
-                     </div>
-                    <div className="grid-td">
-                        <div className="grid-td-padd">
-                            Loan with guarantee
-                        </div>
-                    </div>
-                    <div className="grid-td">
-                        <div className="grid-td-padd">
-                            Have a good reputation
-                        </div>
-                    </div>
-                    <div className="grid-td">
-                        <div className="grid-td-padd">
-                            $10.000.000
-                        </div>
-                    </div>
-                    <div className="grid-td">
-                        <div className="grid-td-padd">
-                            <button>borrow</button>
-                        </div>
-                    </div> 
-
-                    <div className="grid-td">
-                        <div className="grid-td-padd">
-                            Jesús Alberto Coronado       
-                        </div>
-                     </div>
-                    <div className="grid-td">
-                        <div className="grid-td-padd">
-                            Loan with guarantee
-                        </div>
-                    </div>
-                    <div className="grid-td">
-                        <div className="grid-td-padd">
-                            Have a good reputation
-                        </div>
-                    </div>
-                    <div className="grid-td">
-                        <div className="grid-td-padd">
-                            $10.000.000
-                        </div>
-                    </div>
-                    <div className="grid-td">
-                        <div className="grid-td-padd">
-                            <button>borrow</button>
-                        </div>
-                    </div> 
-
-                    <div className="grid-td">
-                        <div className="grid-td-padd">
-                            Jesús Alberto Coronado       
-                        </div>
-                     </div>
-                    <div className="grid-td">
-                        <div className="grid-td-padd">
-                            Loan with guarantee
-                        </div>
-                    </div>
-                    <div className="grid-td">
-                        <div className="grid-td-padd">
-                            Have a good reputation
-                        </div>
-                    </div>
-                    <div className="grid-td">
-                        <div className="grid-td-padd">
-                            $10.000.000
-                        </div>
-                    </div>
-                    <div className="grid-td">
-                        <div className="grid-td-padd">
-                            <button>borrow</button>
-                        </div>
-                    </div> 
-
-                    <div className="grid-td">
-                        <div className="grid-td-padd">
-                            Jesús Alberto Coronado       
-                        </div>
-                     </div>
-                    <div className="grid-td">
-                        <div className="grid-td-padd">
-                            Loan with guarantee
-                        </div>
-                    </div>
-                    <div className="grid-td">
-                        <div className="grid-td-padd">
-                            Have a good reputation
-                        </div>
-                    </div>
-                    <div className="grid-td">
-                        <div className="grid-td-padd">
-                            $10.000.000
-                        </div>
-                    </div>
-                    <div className="grid-td">
-                        <div className="grid-td-padd">
-                            <button>borrow</button>
-                        </div>
-                    </div> 
-                                 
-
+                <div className="grid-content-td">
+                    {this.createTable()}
                 </div>
-              
             </div>
         </div>
 
