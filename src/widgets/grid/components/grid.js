98e424f407f075996f12b13cac2d45edb7daa1b2
jcoronado1 <email.coronado@gmail.com>
2018-12-31 11:31:53 -0500

    ajuste de grid

--- src/widgets/grid/components/grid.js
+++ src/widgets/grid/components/grid.js
@@ -16,16 +16,24 @@ class Grid extends Component{
             <div className="grid">
                 <div className="grid-content-th">
                     <div className="grid-th">
-                        Lender
+                        <div className="grid-th-padd">
+                            Lender    
+                        </div>
                     </div>
                     <div className="grid-th">
-                       Loan Method
+                        <div className="grid-th-padd">
+                            Loan Method     
+                        </div>
                     </div>
                     <div className="grid-th">
-                       Requirements
+                        <div className="grid-th-padd">
+                            Requirements     
+                        </div>
                     </div>
                     <div className="grid-th">
-                        Amount
+                        <div className="grid-th-padd">
+                            Amount    
+                        </div>
                     </div>
                     <div className="grid-th">
                         
@@ -33,67 +41,109 @@ class Grid extends Component{
                 </div>
                 <div className="grid-content-td nuevaclase" id="contenido">
                     <div className="grid-td">
-                        Jesús Alberto Coronado
+                        <div className="grid-td-padd">
+                            Jesús Alberto Coronado       
+                        </div>
+                     </div>
+                    <div className="grid-td">
+                        <div className="grid-td-padd">
+                            Loan with guarantee
+                        </div>
                     </div>
                     <div className="grid-td">
-                        Loan with guarantee
+                        <div className="grid-td-padd">
+                            Have a good reputation
+                        </div>
                     </div>
                     <div className="grid-td">
-                        Have a good reputation
+                        <div className="grid-td-padd">
+                            $10.000.000
+                        </div>
                     </div>
                     <div className="grid-td">
-                        $10.000.000
-                    </div>
-                    <div className="grid-td">
-                        <button>borrow</button>
+                        <div className="grid-td-padd">
+                            <button>borrow</button>
+                        </div>
                     </div> 
 
-                     <div className="grid-td">
-                        Jesús Alberto Coronado
+                    <div className="grid-td">
+                        <div className="grid-td-padd">
+                            Jesús Alberto Coronado       
+                        </div>
+                     </div>
+                    <div className="grid-td">
+                        <div className="grid-td-padd">
+                            Loan with guarantee
+                        </div>
                     </div>
                     <div className="grid-td">
-                        Loan with guarantee
+                        <div className="grid-td-padd">
+                            Have a good reputation
+                        </div>
                     </div>
                     <div className="grid-td">
-                        Have a good reputation
+                        <div className="grid-td-padd">
+                            $10.000.000
+                        </div>
                     </div>
                     <div className="grid-td">
-                        $10.000.000
-                    </div>
-                    <div className="grid-td">
-                        <button>borrow</button>
-                    </div>
+                        <div className="grid-td-padd">
+                            <button>borrow</button>
+                        </div>
+                    </div> 
 
-                     <div className="grid-td">
-                        Jesús Alberto Coronado
+                    <div className="grid-td">
+                        <div className="grid-td-padd">
+                            Jesús Alberto Coronado       
+                        </div>
+                     </div>
+                    <div className="grid-td">
+                        <div className="grid-td-padd">
+                            Loan with guarantee
+                        </div>
                     </div>
                     <div className="grid-td">
-                        Loan with guarantee
+                        <div className="grid-td-padd">
+                            Have a good reputation
+                        </div>
                     </div>
                     <div className="grid-td">
-                        Have a good reputation
+                        <div className="grid-td-padd">
+                            $10.000.000
+                        </div>
                     </div>
                     <div className="grid-td">
-                        $10.000.000
+                        <div className="grid-td-padd">
+                            <button>borrow</button>
+                        </div>
+                    </div> 
+
+                    <div className="grid-td">
+                        <div className="grid-td-padd">
+                            Jesús Alberto Coronado       
+                        </div>
+                     </div>
+                    <div className="grid-td">
+                        <div className="grid-td-padd">
+                            Loan with guarantee
+                        </div>
                     </div>
                     <div className="grid-td">
-                        <button>borrow</button>
+                        <div className="grid-td-padd">
+                            Have a good reputation
+                        </div>
                     </div>
                     <div className="grid-td">
-                        Jesús Alberto Coronado
+                        <div className="grid-td-padd">
+                            $10.000.000
+                        </div>
                     </div>
                     <div className="grid-td">
-                        Loan with guarantee
-                    </div>
-                    <div className="grid-td">
-                        Have a good reputation
-                    </div>
-                    <div className="grid-td">
-                        $10.000.000
-                    </div>
-                    <div className="grid-td">
-                        <button>borrow</button>
-                    </div>                 
+                        <div className="grid-td-padd">
+                            <button>borrow</button>
+                        </div>
+                    </div> 
+                                 
 
                 </div>
               
