ca67e310262297907556229cf9f282c3a8bdd0bd
nicolas lizarazo <nicolizarazo19@hotmail.com>
2019-01-19 09:22:38 -0500

    utility 19 ene

--- src/utility/utility.js
+++ src/utility/utility.js
@@ -2,14 +2,30 @@ import React, { Component} from 'react';
 import { SHOW_USERS } from '../utility/apiRestAction'
 import Services from '../services/services'
 class Utility extends Component{
-
-    SetNames(){
-        const names = Services.request1();
-        key=names.id;
-        console.log(names.name)
-        console.log(names.name)
-        console.log('hello')
+    constructor(props) {
+        super(props);
+        this.state = {            
+        };
+        this.child1=React.createRef();
     }
+call1 = () =>{
+    // this.child1.current.llamado1();
+};
+   render(){
+    return(
+        <div>
+            <Services ref={this.child1}/>
+            <button onClick={this.call1()}>click to make request1</button>            
+            </div>
+        
+        )
+} 
+//  request1(){
+//      axios.get('http://jsonplaceholder.typicode.com/users')
+//              .then((response) => {
+//                 return map(response.data.name)
+//              }) 
+//  }
     
 }
 export default Utility;
